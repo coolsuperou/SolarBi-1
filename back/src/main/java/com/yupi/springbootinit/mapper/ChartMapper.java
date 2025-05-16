92f502202cc14cab331080c83e25755db981edae
coolsuperou <2332371994@qq.com>
2025-05-17 00:59:22 +0800

    feat(图表): 实现图表的增删改查

---
+++ back/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.Chart;
+
+
+/**
+* @author 23323
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2025-05-16 23:24:41
+* @Entity generator.domain.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
--- back/src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
+++ back/src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -3,12 +3,13 @@ package com.yupi.springbootinit.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yupi.springbootinit.model.entity.User;
 
+
 /**
- * 用户数据库操作
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
- */
+* @author 23323
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-05-16 23:47:09
+* @Entity generator.domain.User
+*/
 public interface UserMapper extends BaseMapper<User> {
 
 }

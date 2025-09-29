6344c8237fdbede4bc474914d1ba7083be5d5c17
ozh <2332371994@qq.com>
2025-09-29 13:37:02 +0800

    feat(车间): 完成全部页面并新增宿舍楼

--- front/config/routes.ts
+++ front/config/routes.ts
@@ -10,7 +10,7 @@ export default [
   {
     path: '/airConditioning',
     name: '114_空调水机主机',
-    icon: 'thunderbolt',
+    icon: 'WalletFilled',
     component: './AirConditioning',
     access: 'canUser',
   },
@@ -114,6 +114,90 @@ export default [
     access: 'canUser',
   },
   {
+    path: '/warehouse-113',
+    name: '113仓库',
+    icon: 'inbox',
+    component: './Warehouse113',
+    access: 'canUser',
+  },
+  {
+    path: '/elevator-114',
+    name: '114_2#厂房电梯',
+    icon: 'BankFilled', // 电梯图标
+    component: './Elevator114',
+    access: 'canUser',
+  },
+  {
+    path: '/office-area-114',
+    name: '114_2#楼办公区域',
+    icon: 'home', // 办公楼图标
+    component: './OfficeArea114',
+    access: 'canUser',
+  },
+  {
+    path: '/conference-room-114',
+    name: '114_2#楼会议室',
+    icon: 'team', // 会议室图标
+    component: './ConferenceRoom114',
+    access: 'canUser',
+  },
+  {
+    path: '/laboratory-114',
+    name: '114_2#楼实验室',
+    icon: 'experiment', // 实验室图标
+    component: './Laboratory114',
+    access: 'canUser',
+  },
+  {
+    path: '/public-114',
+    name: '114公共',
+    icon: 'global', // 公共区域图标
+    component: './Public114',
+    access: 'canUser',
+  },
+  {
+    path: '/air-compressor-114',
+    name: '114空压机',
+    icon: 'HddFilled',
+    component: './AirCompressor114',
+    access: 'canUser',
+  },
+  {
+    path: '/charging-pile',
+    name: '充电桩',
+    icon: 'ApiFilled',
+    component: './ChargingPile',
+    access: 'canUser',
+  },
+  {
+    path: '/tool-rd-center',
+    name: '工具研发中心',
+    icon: 'PushpinFilled', // 工具图标，可选：'experiment', 'build', 'setting'
+    component: './ToolRDCenter',
+    access: 'canUser',
+  },
+  {
+    path: '/guard-room',
+    name: '门卫室',
+    icon: 'InsuranceFilled',
+    component: './GuardRoom',
+    access: 'canUser',
+  },
+  {
+    path: '/canteen',
+    name: '食堂',
+    icon: 'shop', // 食堂图标，可选：'coffee', 'restaurant', 'home'
+    component: './Canteen',
+    access: 'canUser',
+  },
+  {
+    path: '/dormitory',
+    name: '宿舍楼',
+    icon: 'home', // 宿舍楼图标，可选：'building', 'bank', 'apartment'
+    component: './Dormitory',
+    access: 'canUser',
+  },
+  {
     path: '/admin',
     icon: 'crown',
     name: '管理页',
@@ -122,6 +206,7 @@ export default [
       { path: '', redirect: 'user' },
       { icon: 'table', path: 'user', component: './Admin/User', name: '用户管理' },
     ],
+
   },
   { path: '/', redirect: '/airConditioning'},
   { path: '*', layout: false, component: './404' },
